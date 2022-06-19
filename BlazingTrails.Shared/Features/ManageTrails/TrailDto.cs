cb9c706033d0a217af85d79c4d45ac2c6837e5c6
Jakub Pietrus <pietrusjakub@gmail.com>
2022-06-19 17:59:31 +0200

    Api fixed

--- BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
+++ BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using BlazingTrails.Shared.Features.ManageTrails.Shared;
+using FluentValidation;
 
 namespace BlazingTrails.Shared.Features.ManageTrails;
 
@@ -11,8 +12,9 @@ public class TrailDto
     public int TimeInMinutes { get; set; }
     public int Length { get; set; }
     public List<RouteInstruction> Route { get; set; } = new List<RouteInstruction>();
+	public ImageAction ImageAction { get; set; }
 
-    public class RouteInstruction
+	public class RouteInstruction
     {
         public int Stage { get; set; }
         public string Description { get; set; } = "";
